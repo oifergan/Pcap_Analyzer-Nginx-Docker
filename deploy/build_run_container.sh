f46793759dcaea1361cbb10e56eb721941fa604b
root <root@localhost.localdomain>
2016-12-25 21:06:27 -0500

    updated deploy script and added current directory as an archive

--- deploy/build_run_container.sh
+++ deploy/build_run_container.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+####### Make sure Docker Engine runs and stops old container ######
+
+sudo systemctl start docker
+sleep 5
+sudo docker stop $(docker ps|grep oifergan:nDPI-Nginx-Docker|awk '{print $1}')
+
 ####### Build the and RUN the Docker nDPI-Nginx-Docker Container ######
 
 cd /tmp/nDPI-Nginx-Docker/
